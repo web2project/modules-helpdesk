15a1f5e2255c7652747bccf9f94b1ff3b3a51cd3
Brian Follas <github@62brains.com>
2015-04-10 02:43:40 -0400

    Fixed path references.

--- vw_idx_my.php
+++ vw_idx_my.php
@@ -3,8 +3,8 @@ if (!defined('W2P_BASE_DIR')) {
     die('You should not access this file directly');
 }
 
-require_once($w2Pconfig['root_dir'] . "helpdesk.functions.php");
-require_once($w2Pconfig['root_dir'] . "vw_idx_handler.php");
+require_once($w2Pconfig['root_dir'] . "/modules/helpdesk/helpdesk.functions.php");
+require_once($w2Pconfig['root_dir'] . "/modules/helpdesk/vw_idx_handler.php");
 
 // Show my items
 vw_idx_handler(2);
