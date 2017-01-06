a3f6853775765c134bf86153b3b2a90c536c4c50
Keith Casey <keith@caseysoftware.com>
2017-01-06 00:20:56 -0600

    restoring the module config

--- helpdesk/helpdesk.functions.php
+++ helpdesk/helpdesk.functions.php
@@ -3,8 +3,7 @@ if (!defined('W2P_BASE_DIR')) {
     die('You should not access this file directly');
 }
 
-require_once($w2Pconfig['root_dir'] . "/modules/helpdesk/helpdesk.functions.php");
-include_once 'config.php';
+include_once($w2Pconfig['root_dir'] . "/modules/helpdesk/config.php");
 
 function getAllowedUsers($companyid=0,$activeOnly=0)
 {
